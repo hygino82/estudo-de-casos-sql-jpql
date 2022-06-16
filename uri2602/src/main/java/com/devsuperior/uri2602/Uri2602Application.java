6801fb7891efb3f0b4975f61f34525eec34f10b6
Adroaldo <hygino@msn.com>
2022-06-16 19:04:15 -0300

    Consulta usando JPQL

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,20 +1,22 @@
 package com.devsuperior.uri2602;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.devsuperior.uri2602.dto.CustomerDTO;
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
 import com.devsuperior.uri2602.services.CustomerService;
 
 @SpringBootApplication
 public class Uri2602Application implements CommandLineRunner {
 
-//	@Autowired
-//	private CustomerRepository repository;
+	@Autowired
+	private CustomerRepository repository;
 
 	@Autowired
 	private CustomerService service;
@@ -25,13 +27,16 @@ public class Uri2602Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		List<CustomerMinProjection> list = repository.buscarApenasNome("RS");
-//		System.out.println("Lista de pessoas");
-//
-//		list.forEach(x -> System.out.println(x.getName()));
+		System.out.println("\nResultado SQL Raiz");
+		var list1 = repository.buscarApenasNome("rs");
+		List<CustomerMinDTO> res1 = list1.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		res1.forEach(x -> System.out.println(x));
+		
+		System.out.println("\n--------------------------------------\n");
 
-		List<CustomerDTO> list = service.buscarPeloEstado("RS");
+		System.out.println("\nResultado JPQL");
+		var list = repository.busca02Jpql("rs");
+
 		list.forEach(x -> System.out.println(x));
-
 	}
 }

b410a0ab757a2faeb078fc5c9b3a7064f3db65a6
Adroaldo <hygino@msn.com>
2022-06-19 21:41:05 -0300

    Uri 2609 SQL e JPQL

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -12,7 +17,7 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2609Application.class, args);
 	}
@@ -20,5 +25,14 @@ public class Uri2609Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		List<CategorySumProjection> list = repository.buscarPorCategoriaSoma();
+		List<CategorySumDTO> res = list.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+		System.out.println("Resultado usando SQL Raiz");
+		res.forEach(x -> System.out.println(x));
+
+		System.out.println("\n----------------------------------------------------\n");
+		System.out.println("Resultado usando JPQL");
+		List<CategorySumDTO> res1 = repository.jpqlBuscarPorCategoriaSoma();
+		res1.forEach(x -> System.out.println(x));
 	}
 }

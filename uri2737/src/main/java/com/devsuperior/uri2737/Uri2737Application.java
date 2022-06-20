c7daad055da0c2b49fd62072d196d3bcab309c0c
Adroaldo <hygino@msn.com>
2022-06-20 11:11:46 -0300

    Uri 2737 SQL

--- uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2737;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
 @SpringBootApplication
@@ -12,14 +17,17 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Autowired
 	private LawyerRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2737Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-
+		List<LawyerMinProjection> list = repository.buscarAdvogadosComMaximoClientesMininoMedia();
+		List<LawyerMinDTO> res = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+		System.out.println("Resultado usando SQL Raiz");
+		res.forEach(x -> System.out.println(x));
+		System.out.println("\n----------------------------------------------------\n");
 	}
 }

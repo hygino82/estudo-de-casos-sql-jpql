e56ce4ca695d58a4152a37edc34e6ecb72eba895
Adroaldo <hygino@msn.com>
2022-06-20 16:48:50 -0300

    Uri 2990 SQL e JPQL

--- uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2990;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
 @SpringBootApplication
@@ -12,7 +17,7 @@ public class Uri2990Application implements CommandLineRunner {
 
 	@Autowired
 	private EmpregadoRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2990Application.class, args);
 	}
@@ -20,5 +25,21 @@ public class Uri2990Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		System.out.println("Resultado usando SQL Raiz NOT IN");
+		List<EmpregadoDeptProjection> list = repository.buscaEmpregadosSemProjetos();
+		List<EmpregadoDeptDTO> res = list.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		res.forEach(x -> System.out.println(x));
+		
+		System.out.println("\n-------------------------------------------\n");
+		
+		System.out.println("Resultado usando JPQL");
+		List<EmpregadoDeptDTO> res1 = repository.jpqlBuscaEmpregadosSemProjetos();
+		res1.forEach(x -> System.out.println(x));
+		
+		System.out.println("\n-------------------------------------------\n");
+		System.out.println("Resultado usando SQL Raiz LEFT JOIN");
+		List<EmpregadoDeptProjection> list2 = repository.buscaEmpregadosSemProjetosV2LeftJoin();
+		List<EmpregadoDeptDTO> res2 = list2.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
+		res2.forEach(x -> System.out.println(x));
 	}
 }
